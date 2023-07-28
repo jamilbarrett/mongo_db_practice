364ee28ea5699b78c5f41e6bf3ae3648b1179fe4
Jamil Barrett <jamilbarrett@Jamils-iMac.local>
2023-07-28 09:38:53 -0400

    fixed

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const isProducitn = process.env.PORT
+const isProduction = process.env.PORT
 if (isProduction) {
     mongoose.connect(`mongodb+srv://job:${process.env.DB_PASSWORD}@cluster0.cquceqx.mongodb.net/?retryWrites=true&w=majority`)
 } else mongoose.connect('mongodb://127.0.0.1:27017/planets_api_db');
